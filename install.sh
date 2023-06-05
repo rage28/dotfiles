1d1be4b191e425a0454649247a54df7b599d741c
Raghavendra Bhuvan <rage28@gmail.com>
2023-06-05 18:15:59 +0530

    Remove installation of nix and nix-darwin

--- install.sh
+++ install.sh
@@ -3,37 +3,28 @@
 # setops
 set -e
 
-# vars
-declare -r DOTFILES="~/.dotfiles"
-source /etc/bashrc
+# check nix
+# nix
+command -v "nix" &>/dev/null || {
+  printf "\nnix not found. Please install nix and try again. For more information check https://nixos.org/download.html#nix-install-macos\n\n"
+  exit 1
+}
+
+command -v "darwin-rebuild" &>/dev/null || {
+  printf "\nnix-darwin not found. Please install nix-darwin and try again. For more information check https://github.com/LnL7/nix-darwin#install\n\n"
+  exit 1
+}
 
 clear
 
 # chezmoi
 command -v "chezmoi" &>/dev/null || {
-  printf "Installing and setting up chezmoi and your dotfiles\n\n"
+  printf "\nInstalling and setting up chezmoi and your dotfiles\n"
+  printf "===================================================\n"
   sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply ${GITHUB_USERNAME:-rage28}
 }
 
-# nix
-command -v "nix" &>/dev/null || {
-  printf "\n\nInstalling nix\n\n"
-  curl -L https://nixos.org/nix/install | sh
-  source /etc/bashrc
-}
-
-# nix darwin
-command -v "darwin-rebuild" &>/dev/null || {
-  printf "\n\nInstalling nix-darwin\n\n"
-  
-  pushd ${DOTFILES} &>/dev/null
-
-  nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
-  .${DOTPWD}/result/bin/darwin-installer
-
-  # update rc after nix-darwin to wait for /etc/static
-  source /etc/bashrc 
-  darwin-rebuild switch
-
-  popd &>/dev/null
-}
+# rebuild darwin config
+printf "\nRebuilding nix-darwin configuration\n"
+printf "===================================\n"
+darwin-rebuild switch
