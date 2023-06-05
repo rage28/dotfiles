7e38741fec36555e00aecad62778ca049030c479
Raghavendra Bhuvan <rage28@gmail.com>
2023-06-05 16:44:49 +0530

    Install chezmoi via shell, and nix-darwin

--- install.sh
+++ install.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash
 
-builtin command -v "nix" &>/dev/null || {
+# setops
+set -e
+
+# vars
+declare -r DOTPWD=$(pwd -P)
+
+clear
+
+# chezmoi
+command -v "chezmoi" &>/dev/null || {
+  printf "Installing and setting up chezmoi and your dotfiles\n\n"
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply ${GITHUB_USERNAME:-rage28}
+}
+
+# nix
+command -v "nix" &>/dev/null || {
+  printf "\n\nInstalling nix\n\n"
   curl -L https://nixos.org/nix/install | sh
 }
 
-__nix_sync_packages() {
-  packages=(
-    nixpkgs.zsh
-    nixpkgs.starship
-    nixpkgs.chezmoi
-  )
+# nix darwin
+command -v "darwin-rebuild" &>/dev/null || {
+  printf "\n\nInstalling nix-darwin\n\n"
+  nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+  ./${DOTPWD}/result/bin/darwin-installer
 
-  nix-env -iA ${packages[@]}
+  # update rc after nix-darwin to wait for /etc/static
+  source /etc/bashrc 
+  darwin-rebuild switch
 }
-
-builtin alias nixsync=__nix_sync_packages
-
-# sync packages
-nixsync
